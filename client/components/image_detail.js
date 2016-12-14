b724900697d3416315091a89b056e3518f9f8249
zzpengg <cfps91152@gmail.com>
2016-12-14 22:51:03 +0800

    add progress-bar

--- client/components/image_detail.js
+++ client/components/image_detail.js
@@ -1,6 +1,5 @@
-
-
 import React from 'react';
+import ImageScore from './image_score';
 
 const ImageDetail = (props) => {
   // props.image => this is the image object
@@ -16,6 +15,8 @@ const ImageDetail = (props) => {
         <h4 className="media-heading">
           {props.image.title}
         </h4>
+        <p>{props.image.description}</p>
+        <ImageScore ups={props.image.ups} downs={props.image.downs}/>
       </div>
     </li>
   );
