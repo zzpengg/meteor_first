a02c362afa5cbaa225b8c0bebcd840c072e2dee7
zzpengg <cfps91152@gmail.com>
2016-12-14 22:17:17 +0800

    24

--- client/main.js
+++ client/main.js
@@ -1,16 +1,36 @@
 // this js is automatically loaded
 
 // Import the react library
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import ImageList from './components/image_list';
+import Axios from 'axios';
 
 // Create a component
-const App = () => {
-  return (
-    <div>
-      React App #2
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { images: [] };
+  }
+
+
+  componentWillMount(){
+    // fantastic place to load data
+    console.log('app is about to render');
+    Axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+      .then( response => this.setState({ images: response.data.data }) );
+      // Never Do This -
+      // this.state.images = [ {}, {} ];
+  }
+
+  render(){
+    return (
+      <div>
+        <ImageList images={this.state.images}/>
+      </div>
+    );
+  };
 };
 
 //Render the component to the screen
